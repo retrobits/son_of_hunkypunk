3a884d598a1464e7f6c3597d7a08fb2cb7556869
Rafal Rzepecki <divided.mind@gmail.com>
2009-09-19 13:24:54 +0200

    Initial commit.

---
+++ src/org/andglk/GlkTest.java
@@ -0,0 +1,13 @@
+package org.andglk;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GlkTest extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
