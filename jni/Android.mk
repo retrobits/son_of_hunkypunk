e4a6be41791d455625ddd53ffa0d4ac342208fe5
Rafał Rzepecki <divided.mind@gmail.com>
2009-10-06 00:17:10 +0200

    Remove bison generated file. Fix log library path.

--- jni/Android.mk
+++ jni/Android.mk
@@ -5,6 +5,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := andglk
 LOCAL_SRC_FILES := andglk.c gi_dispa.c
 LOCAL_CPPFLAGS	:= -W -Wall
-LOCAL_LDLIBS	:= -llog -L/home/divide/.local/android-ndk-1.5_r1/build/platforms/android-1.5/arch-arm/usr/lib/
+LOCAL_LDLIBS	:= -llog -Lbuild/platforms/android-1.5/arch-arm/usr/lib/
 
 include $(BUILD_STATIC_LIBRARY)
