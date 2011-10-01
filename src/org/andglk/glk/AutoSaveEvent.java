c53a8a2d71fd4050d283b916797809e3bec9a38d
Dan Vernon <dtvernon@gmail.com>
2011-10-01 09:29:02 -0700

    new events for autosave & exit

---
+++ src/org/andglk/glk/AutoSaveEvent.java
@@ -0,0 +1,31 @@
+/*
+	Copyright © 2009 Rafał Rzepecki <divided.mind@gmail.com>
+
+	This file is part of Hunky Punk.
+
+    Hunky Punk is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Hunky Punk is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Hunky Punk.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package org.andglk.glk;
+
+public class AutoSaveEvent extends Event {
+	public String FileName = "";
+	public int LineEvent = 0;
+
+	public AutoSaveEvent(Window w, String fileName, int lineEvent) {
+		super(w);
+		FileName = fileName;
+		LineEvent = lineEvent;
+	}
+}
